ba7d21c8e7a329edd9e799875bc647af8b2e7650
fajar4561 <maulanafajar751@gmail.com>
2021-10-29 07:35:00 +0700

    up

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ DEFCONFIG=X00TD_defconfig
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
-NAMA=Signature
+NAMA=perf
 
 JENIS=EAS
 
@@ -65,7 +65,7 @@ BUILD_TYPE="Dirty"
 
 # Specify compiler.
 # 'clang' or 'clangxgcc' or 'gcc' or 'gcc49'
-COMPILER=clang
+COMPILER=gcc49
 
 # Message on anykernel when installation
 MESSAGE="don't blame me if u get poor battery backup or weak performance . i'm not responsible . Do with Your Own Risk."
